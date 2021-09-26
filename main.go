a9009aed2ec1f197ae267ccdd76c6b4937dc0007
lennypus <lenny.cipondoh@gmail.com>
2021-09-26 11:22:07 +0700

    try action workflow for go build

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/brianrainer/makefile-tutorial/calculator"
 )
 
-func main(){
-	fmt.Println("Hello!")
-	fmt.Println(calculator.Add(5,3))
+func main() {
+	fmt.Println("Hello! Aku Coba Build pakai action workflow")
+	fmt.Println(calculator.Add(5, 3))
 }
